01d4a24c05e3471707889420a510949275e035d9
Om <miaraoren@gmail.com>
2017-07-30 12:29:20 +0300

    'First'

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname +'/views/partials');
+
+// Set express view engine to use hbs moudle
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to file');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+      res.render('Maintanance.hbs');
+});
+
+// Declare Static folder for public use & view
+app.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// Get homepage
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hello & Welcome to my website',
+    });
+});
+
+// Get aboutpage
+app.get('/about', (req, res) => {
+   res.render('about.hbs', {
+       pageTitle : 'About Page',
+   }); 
+});
+
+
+// Start server by listening to port 8080
+app.listen(8080);
+console.log("Server is Running...");
